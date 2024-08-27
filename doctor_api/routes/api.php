d763f19c3c5e9c957db8fccf0704237aa38eb447
edo2019 <eddyyospher@gmail.com>
2024-08-27 15:12:08 +0300

    signup page

--- doctor_api/routes/api.php
+++ doctor_api/routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[UsersController::class, 'login']);
+Route::post('/register',[UsersController::class, 'register']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
